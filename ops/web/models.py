7e52a9e944df9ec7a020e1c3ac7b0ea236f2504c
吴伟01 <wei.wu01@weimob.com>
2019-04-17 15:35:17 +0800

    init

---
+++ ops/web/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class UserSystem(models.Model):
+    # 姓名
+    name = models.CharField(max_length=30)
+    # 密码
+    password = models.CharField(default='123456', max_length=20)
+    # 登陆状态 默认0：离线  1:登陆
+    status = models.IntegerField(max_length=1, default=0)
+    homedirectory = models.CharField(max_length=30)
+    groupname = models.CharField(max_length=30)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'user_sys'  # 自定义表名称为user_sys
+        verbose_name = '系统用户'  # 指定在admin管理界面中显示的名称
+        ordering = ('create_time',)
